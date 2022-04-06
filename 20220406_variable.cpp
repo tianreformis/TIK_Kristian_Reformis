980c3a644a0b0325c73f6c8b0c02053c7377a201
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2022-04-06 11:08:44 +0800

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream> 
+using namespace std;
+int main() {//begin of function
+    float angka;
+    string nama; //deklarasi variabel
+    nama = "Tian Reformis"; //masukkan value kedalam variabel
+    angka = 10.21;
+    cout<<nama<<endl; //menampilkan variabel
+    cout<<angka+3.14;
+    
+}//end of function
