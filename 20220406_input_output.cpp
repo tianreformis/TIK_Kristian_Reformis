87e8078914d36325bc7731c60dc38895c3f597ac
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2022-04-06 11:26:46 +0800

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream> 
+using namespace std;
+int main() {//begin of function
+    string nama;
+    cout<<"Masukkan Nama: "; cin>>nama;
+    cout<<"Nama Anda: "<<nama;
+    
+    
+}//end of function
